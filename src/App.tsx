d88f0f8b955b5c7644f0120676514538eafde6c6
Alex Williamson <arthurwilliamson@gmail.com>
2023-11-07 14:56:14 -0500

    remove vite styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
-
+  console.log('In App')
   return (
-    <div className="container">
+    <div>
       Alex Williamson
+      <p>Hello there!</p>
     </div>
   )
 }
