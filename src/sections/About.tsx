abc197075a94733bdc1dc12031a0ce6eaf8992da
Alex Williamson <arthurwilliamson96@gmail.com>
2023-11-09 11:18:25 -0500

    updated about me more

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
   console.log(showMore)
 
   return (
-    <section id="about">
+    <section className="pt-8" id="about">
       <div className="hero">
         <div className="hero-content flex-col lg:flex-row">
           <img src="./assets/alex.jpg" className="rounded-lg shadow-2xl sm:w-full" />
@@ -20,22 +20,25 @@ const About = () => {
         </div>
       </div>
       <div className="flex align-middle justify-center ">
-        {showMore && <div className="text-2xl sm:w-3/5 justify-start p-4 bg-base-100 rounded-lg">
-        <p className="pb-8">
-              Thanks for visiting my website! Let me tell you more about myself. I am a full-stack developer. On the backend, I mostly work with Spring Boot, and on the frontend, I specialize in Angular and React.
-            </p>
-            <p className="pb-8">
-              After graduating from UGA, I worked as a chemical engineer. While I enjoyed the work, I was more intrigued by the data and programming aspect of my job. I realized I wanted to pursue a career in programming, so I taught myself Python and made some projects after work. Eventually, I landed a job at FedEx.
-            </p>
-            <p className="pb-8">
-              I am currently enrolled at Georgia Tech, where I am getting a Masters in computer science. Being able to fill in the knowledge gaps from my self-teaching has been extremely meaningful.
-            </p>
-            <p className="pb-8">
-              In my free time, I like to go backpacking, play chess, and bake. Feel free to reach out to me about my professional experience or to hear about my latest hike or recipe.
-            </p>
-            <p className="pb-8">
-              You can <a className="text-primary underline hover:text-primary-focus" href="mailto:arthurwilliamson96@gmail.com">email me</a> or message me directly on <a className="text-primary underline hover:text-primary-focus" href="https://www.linkedin.com/in/alex-williamson-dev">LinkedIn</a>.
-            </p>
+        {showMore && <div className="text-2xl sm:w-3/4 justify-start p-4 bg-base-100 rounded-lg">
+          <p className="pb-8">
+            Thanks for visiting my website! Let me tell you more about myself. I am a full-stack developer. On the backend, I mostly work with Spring Boot, and on the frontend, I specialize in Angular and React.
+          </p>
+          <p className="pb-8">
+            After graduating from UGA, I worked as a chemical engineer. While I enjoyed the work, I was more interested by the data and programming aspect of my jobs.
+            I realized I wanted to pursue a career in programming, so I taught myself Python and started making projects in my free time.
+            After months of learning and working on projects, I landed a job at FedEx.
+          </p>
+          <p className="pb-8">
+            I am currently enrolled at Georgia Tech in their OMSCS program, where I am getting a Masters in Computer Science.
+            Being able to fill in the knowledge gaps from my self-teaching has been extremely meaningful and is helping me to become a better software engineer.
+          </p>
+          <p className="pb-8">
+            In my free time, I like to go backpacking, play chess, and bake. Feel free to reach out to me about my professional experience or to hear about my latest hike or recipe.
+          </p>
+          <p className="pb-8">
+            You can <a className="text-primary underline hover:text-primary-focus" href="mailto:arthurwilliamson96@gmail.com">email me here</a> or message me directly on <a className="text-primary underline hover:text-primary-focus" href="https://www.linkedin.com/in/alex-williamson-dev">LinkedIn</a>.
+          </p>
         </div>}
 
       </div>
