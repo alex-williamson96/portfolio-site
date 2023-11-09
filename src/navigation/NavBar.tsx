16c9131e5bb496b0a28896151b715a14e1f4c47d
Alex Williamson <arthurwilliamson96@gmail.com>
2023-11-09 10:16:44 -0500

    added light and dark mode

--- src/navigation/NavBar.tsx
+++ src/navigation/NavBar.tsx
@@ -1,3 +1,5 @@
+import Theme from "../ui/Theme";
+
 const NavBar = () => {
 
   const goToSection = (id: string) => {
@@ -14,12 +16,12 @@ const NavBar = () => {
   }
 
   return (
-    <div className="navbar bg-base-100 text-neutral-content fixed justify-center z-50 pl-4 pr-4 w-full">
+    <div className="navbar bg-base-100 text-primary fixed justify-center z-50 pl-4 pr-4 w-full">
       <button className="btn btn-ghost normal-case sm:text-3xl text-s" onClick={() => goToSection('about')}>About</button>
       <button className="btn btn-ghost normal-case sm:text-3xl text-s" onClick={() => goToSection('skills')}>Skills</button>
       <button className="btn btn-ghost normal-case sm:text-3xl text-s" onClick={() => goToSection('projects')}>Projects</button>
       <button className="btn btn-ghost normal-case sm:text-3xl text-s" onClick={() => goToSection('resume')}>Resume</button>
-
+      <Theme />
     </div>
   )
 }
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -15,19 +15,32 @@ const About = () => {
             <p className="lg:py-6 md:py-4 sm:py-2 sm:text-2xl pl-4 text-2xl pb-4">I currently work at FedEx where I use my skills to
               create and improve internal tools used for tracking incidents across the organization.
             </p>
-            <button className="btn btn-primary" onClick={() => setShowMore(true)}>See more about me</button>
+            <button className="btn btn-primary" onClick={() => setShowMore(!showMore)}>See {showMore ? 'less' : 'more'} about me</button>
           </div>
         </div>
       </div>
-      <div className="flex align-middle justify-center">
-        <div className="text-2xl sm:w-3/5 justify-start p-4">
+      <div className="flex align-middle justify-center ">
+        {showMore && <div className="text-2xl sm:w-3/5 justify-start p-4 bg-base-100 rounded-lg">
           <p className="p-2">
             Thanks for visiting my website, let me tell you more about myself. I am a full-stack developer. On the backend, I mostly worth with
-            Spring Boot, and on the frontend I specialize in Angular and React. I am passionate about building code that is scalable and testable
-            within an enterprise environment.
+            Spring Boot, and on the frontend I specialize in Angular and React. I am passionate about building code that is scalable, testable,
+            and intuitive within an enterprise environment.
           </p>
-          <p className="p-2">In my free time, I like to go backpacking, play chess, and bake.</p>
-        </div>
+          <p className="p-4"></p>
+          <p className="p-2">After graudating from UGA, I worked as a chemical engineer. While I enjoyed the work, I was more intrigued by
+            the data and programming aspect of my job. I realized I wanted to pursue a career in programming, so I taught myself Python
+            and made some projects after work and eventually landed a job at FedEx,</p>
+          <p className="p-4"></p>
+          <p className="p-2">I am also currenlty enrolled at Gerogia Tech where I am getting a Masters in computer science. Being able to
+            fill in the knowledge gaps from my self teaching has been extremely meaningful.</p>
+          <p className="p-4"></p>
+          <p className="p-2">In my free time, I like to go backpacking, play chess, and bake. Feel free to reach out to me about my professional
+            experience or to hear about my latest hike or recipe.</p>
+          <p className="p-4"></p>
+          <p className="p-2">You can <a className="text-primary underline hover:text-primary-focus"
+            href="mailto:arthurwilliamson96@gmail.com">email me</a> or message me directly on <a className="text-primary underline hover:text-primary-focus" href="https://www.linkedin.com/in/alex-williamson-dev">LinkedIn.</a></p>
+        </div>}
+
       </div>
     </section>
   )
