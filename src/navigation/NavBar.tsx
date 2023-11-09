c838de5de8a9fdad5d2dd895e61c4ae7f4364de7
Alex Williamson <arthurwilliamson@gmail.com>
2023-11-08 22:28:53 -0500

    updated footer

--- src/navigation/NavBar.tsx
+++ src/navigation/NavBar.tsx
@@ -15,11 +15,10 @@ const NavBar = () => {
 
   return (
     <div className="navbar bg-base-100 text-neutral-content fixed justify-center z-50 pl-4 pr-4 w-full">
-      <button className="btn btn-ghost normal-case sm:text-lg text-xs" onClick={() => goToSection('about')}>About</button>
-      <button className="btn btn-ghost normal-case sm:text-lg text-xs" onClick={() => goToSection('skills')}>Skills</button>
-      <button className="btn btn-ghost normal-case sm:text-lg text-xs" onClick={() => goToSection('projects')}>Projects</button>
-      <button className="btn btn-ghost normal-case sm:text-lg text-xs" onClick={() => goToSection('resume')}>Resume</button>
-      <button className="btn btn-ghost normal-case sm:text-lg text-xs" onClick={() => goToSection('contact')}>Contact</button>
+      <button className="btn btn-ghost normal-case sm:text-3xl text-s" onClick={() => goToSection('about')}>About</button>
+      <button className="btn btn-ghost normal-case sm:text-3xl text-s" onClick={() => goToSection('skills')}>Skills</button>
+      <button className="btn btn-ghost normal-case sm:text-3xl text-s" onClick={() => goToSection('projects')}>Projects</button>
+      <button className="btn btn-ghost normal-case sm:text-3xl text-s" onClick={() => goToSection('resume')}>Resume</button>
 
     </div>
   )
