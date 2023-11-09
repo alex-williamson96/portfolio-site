23f7f592bb98fed4c8011aaafc47508071e07c25
Alex Williamson <arthurwilliamson@gmail.com>
2023-11-09 08:06:14 -0500

    fixed typo

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer className="footer items-center p-4 bg-neutral text-neutral-content sm:pl-32 sm:pr-32">
       <aside className="items-center grid-flow-col">
-        <p>Copyright © 2023 - All right reserved
+        <p>Copyright © 2023 - All rights reserved
         </p>
         <div className="p-2"></div>
         <a href="www.linkedin.com/in/alex-williamson-dev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"></path></svg>
