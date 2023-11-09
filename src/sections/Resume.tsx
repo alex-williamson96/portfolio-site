f30357f5613e534755e79640ba4b2dfd82d5500d
Alex Williamson <arthurwilliamson@gmail.com>
2023-11-09 07:59:55 -0500

    fix all routes

--- src/sections/Resume.tsx
+++ src/sections/Resume.tsx
@@ -5,7 +5,7 @@ const Resume = () => {
         <h1 className="sm:text-6xl text-2xl p-4">Resume</h1>
       </div>
       <div className="md:pr-72 md:pl-72 h-5/6 pl-4 pr-4">
-        <object width="100%" height="100%" data="/src/assets/resume.pdf" type="application/pdf">   </object>
+        <object width="100%" height="100%" data="./assets/resume.pdf" type="application/pdf">   </object>
       </div>
 
     </section>
