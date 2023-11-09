15a5091ff893460b247e6181b797638c1b156c8c
Alex Williamson <arthurwilliamson@gmail.com>
2023-11-09 07:22:29 -0500

    updated file paths

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
     <section className="lg:p-16 md:p-8 sm:p-2" id="about">
       <div className="hero">
         <div className="hero-content flex-col lg:flex-row">
-          <img src="/src/assets/alex.jpg" className="rounded-lg shadow-2xl sm:w-full" />
+          <img src="/porfolio-site/src/assets/alex.jpg" className="rounded-lg shadow-2xl sm:w-full" />
           <div>
             <h1 className="lg:text-6xl font-bold sm:text-4xl md:text-6xl">Hi, I'm Alex Williamson a full-stack developer</h1>
             <p className="lg:py-6 md:py-4 sm:py-2 lg:text-xl md:text-xl sm:text-lg pl-4">I currently work at FedEx where I use my skills to
