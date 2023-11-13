fcbfe946be538ac50af19e193a5b88ca996e400b
Alex Williamson <arthurwilliamson96@gmail.com>
2023-11-13 12:46:10 -0500

    removed picture

--- src/ui/SkillCard.tsx
+++ src/ui/SkillCard.tsx
@@ -1,4 +1,4 @@
-const SkillCard = (props: { name: string, category: string, onSkillSelect: (category: string) => void, selectedSkill: string}) => {
+const SkillCard = (props: { name: string, category: string, onSkillSelect: (category: string) => void, selectedSkill: string }) => {
 
   const defaultStyles = 'flex justify-center items-center p-3 sm:p-6  border-neutral bg-primary hover:bg-primary-focus text-primary-content border-2 sm:text-3xl rounded-lg text-lg'
 
