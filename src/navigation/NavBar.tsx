b100cbfd9a530fbf9d72eb1b1f2b168547955553
Alex Williamson <arthurwilliamson96@gmail.com>
2025-10-22 22:55:56 -0400

    remove projects section from navbar

--- src/navigation/NavBar.tsx
+++ src/navigation/NavBar.tsx
@@ -19,7 +19,7 @@ const NavBar = () => {
     <div className="navbar bg-base-200 text-base-content fixed justify-center z-50 pl-4 pr-4 w-full">
       <button className="btn btn-ghost normal-case sm:text-3xl text-s" onClick={() => goToSection('about')}>About</button>
       <button className="btn btn-ghost normal-case sm:text-3xl text-s" onClick={() => goToSection('skills')}>Skills</button>
-      <button className="btn btn-ghost normal-case sm:text-3xl text-s" onClick={() => goToSection('projects')}>Projects</button>
+      {/* <button className="btn btn-ghost normal-case sm:text-3xl text-s" onClick={() => goToSection('projects')}>Projects</button> */}
       <button className="btn btn-ghost normal-case sm:text-3xl text-s" onClick={() => goToSection('resume')}>Resume</button>
       <Theme />
     </div>
